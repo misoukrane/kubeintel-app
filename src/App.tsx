5382cd682fcb6f32cb32d03b24126de5e6dbffb0
Mohamed Isoukrane <isoukrane.mohamed@gmail.com>
2025-01-17 23:00:25 -0500

    clean up css & app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-
-import "./App.css";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
@@ -16,24 +13,8 @@ function App() {
   }
 
   return (
-    <main className="container">
-      <h1 className="text-3xl font-bold">Hello world!</h1>
-      <Button>Click me</Button>
-      <h1>Welcome to Tauri + React</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold content-center">Welcome to kubeintel app!</h1>
       <form
         className="row"
         onSubmit={(e) => {
@@ -41,15 +22,18 @@ function App() {
           greet();
         }}
       >
+        <div>
         <Input
           id="greet-input"
           onChange={(e) => setName(e.currentTarget.value)}
           placeholder="Enter a name..."
         />
         <Button type="submit">Greet</Button>
+        </div>
+        
       </form>
       <p>{greetMsg}</p>
-    </main>
+    </div>
   );
 }
 
