04159f20dfaa533e0ef3c0457fe91eec06e5c17d
Mohamed Isoukrane <isoukrane.mohamed@gmail.com>
2025-01-20 09:45:46 -0500

    set default path for kubeconfig file picker

--- src/pages/kubeconfig-file-picker.tsx
+++ src/pages/kubeconfig-file-picker.tsx
@@ -1,22 +1,27 @@
 import { useState } from 'react';
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { homeDir, join } from '@tauri-apps/api/path';
 import { open } from '@tauri-apps/plugin-dialog';
-import { FolderOpen } from "lucide-react";
+import { FolderOpen, ArrowRight } from "lucide-react";
 
 const KubeconfigFilePicker = () => {
   const [selectedFile, setSelectedFile] = useState("");
 
   const openFileDialog = async () => {
     try {
+      const homeDirectory = await homeDir();
+      const kubePath = await join(homeDirectory, '.kube');
       const selected = await open({
         multiple: false,
+        directory: false,
+        defaultPath: kubePath,
         filters: [{
           name: 'All Files',
           extensions: ['*.*']
         }]
       });
-      
+
       if (selected) {
         setSelectedFile(selected);
       }
@@ -27,30 +32,33 @@ const KubeconfigFilePicker = () => {
 
   return (
     <div className="flex flex-col h-screen justify-center items-center">
-    <Card className="w-full max-w-md mx-auto">
-      <CardHeader>
-        <CardTitle>Select a kubeconfig file</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <Button 
-          onClick={openFileDialog}
-          variant="outline" 
-          className="w-full p-8 border-dashed border-2 hover:border-primary"
-        >
-          <FolderOpen className="mr-2 h-6 w-6" />
-          Click to select a file
-        </Button>
-      </CardContent>
-      <CardFooter className="flex flex-col items-start">
-        {selectedFile && (
-          <div className="text-sm text-muted-foreground break-all">
-            Selected: {selectedFile}
-          </div>
-        )}
-      </CardFooter>
-    </Card>
+      <Card className="w-full max-w-md mx-auto">
+        <CardHeader>
+          <CardTitle>Choose your kubeconfig file</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Button
+            onClick={openFileDialog}
+            variant="outline"
+            className="w-full p-8 border-dashed border-2 hover:border-primary"
+          >
+            <FolderOpen className="mr-2 h-6 w-6" />
+            Click to select a file
+          </Button>
+        </CardContent>
+        <CardFooter className="flex flex-col items-center justify-center">
+          {selectedFile && (
+            <>
+            <div className="text-sm font-bold break-all">
+              Selected: {selectedFile}
+            </div>
+            <Button className='my-2'>Continue <ArrowRight /></Button>
+            </>
+          )}
+        </CardFooter>
+      </Card>
     </div>
   );
 };
 
-export {KubeconfigFilePicker}; 
+export { KubeconfigFilePicker }; 
