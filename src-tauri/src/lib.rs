0cbb4bd693779cb38dfa2810ced2c4466ad76db4
Mohamed Isoukrane <isoukrane.mohamed@gmail.com>
2025-01-24 13:20:36 -0500

    read kubeconfig file using rust

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,15 +1,17 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn read_kubeconfig(filepath: &str) -> Result<String, String> {
+    std::fs::read_to_string(filepath).map_err(|e| e.to_string())
 }
+
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![read_kubeconfig])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/pages/cluster.tsx
+++ src/pages/cluster.tsx
@@ -1,16 +1,41 @@
 import { Button } from "@/components/ui/button";
 import { useConfigStore } from "@/stores/use-config-store";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
+import { invoke } from '@tauri-apps/api/core';
 
 
 export const Cluster = () => {
-  const  cfgState = useConfigStore();
+  const cfgState = useConfigStore();
+  const [content, setContent] = useState<string>('');
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const readConfig = async () => {
+      // use tauri to read the kubeconfig file
+      const filePath = cfgState.selectedKubeconfig
+      if (!filePath || filePath.length === 0) {
+        navigate('/');
+        return;
+      }
+      try {
+        // tauri invoke command read_kubeconfig
+        const content = await invoke<string>('read_kubeconfig', { filepath: filePath });
+        setContent(content);
+      } catch (error) {
+        console.error('Error reading file:', error);
+        setContent(`Error reading kubeconfig file: ${error}`);
+      }
+    };
+    readConfig();
+  }, []);
+
   return (
     <div>
       <h1>Cluster</h1>
       <h2>{cfgState.selectedKubeconfig}</h2>
       <Button onClick={() => navigate('/')}>config</Button>
+      <pre>{content}</pre>
     </div>
   )
 };
